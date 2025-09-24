05f3fef77c33bd3910c043e9aac62a3319b7f047
Jiali Sheng <jerry_lovae@Jialis-MacBook-Air.local>
2025-09-24 14:44:15 -0700

    Update 02_triangle.py

--- Week04/02_triangle.py
+++ Week04/02_triangle.py
@@ -10,4 +10,9 @@ def main(height):
         print('*' * row) # For each row, print * based on number of rows
         # "string" * int means repeat the "string" for int times
 
-main(int(sys.argv[1]))
+main(int(sys.argv[1])) 
+# In terminal, we input"python 02_triangle.py 5"
+# python 02_triangle has index 0
+# 5 has index 1
+# sys.argv[1]  means in the terminal input, we are looking for the element with the index 1 and pass into the program
+# main(int(5)) will initiate the function and treat 5 as the height parameter
