00b7e0e9a1d126e89f8e202ad5d333220e1c0d3b
Jiali Sheng <jerry_lovae@Jialis-MacBook-Air.local>
2025-10-29 15:01:41 -0700

    Add notes

--- Week09/card_compare/card.py
+++ Week09/card_compare/card.py
@@ -7,7 +7,39 @@ class Card:
     def __str__(self):
         """Return a user friendly string for the card"""
         return self.rank + " of " + self.suit
+    
+    def __repr__(self):
+        return (f"{self.__class__.__name__}:"  # Access the name of the object
+                f"(suit: {self.suit}, rank: {self.rank})")
+    # What is __repr__?
 
+    def __eq__(self, other):
+        """
+        Return a boolean representing equality of cards
+        Other is another card
+        """
+        return (self.rank == other.rank and self.suit == other.suit)
+
+    def __lt__(self, other):
+        """
+        Return a boolean representing less than
+        """
+        return (self.num_value < other.num_value)
+    
+    def __gt__(self, other):
+        """
+        Return a boolean representing less than
+        """
+        return (self.num_value > other.num_value)
+    
+    def __hash__(self):
+        """
+        Define objects' hash function so that the object
+        can be used as the key of the dictionary.
+        """
+        return hash((self.suit, self.rank))
+        # make sure hash function is consistant with __eq__ 
+    
     @property
     def num_value(self):
         """Return the numerical value for the card"""
--- Week09/card_compare/card_compare.py
+++ Week09/card_compare/card_compare.py
@@ -16,32 +16,38 @@ def main():
     # print(repr(card1))
     # print(repr(card2))
 
-    # if card1 == card2:
-    #     print(card1, "and", card2, "are equal")
-    # else:
-    #     print(card1, "and", card2, "are not equal")
-    #     if (card1 > card2):
-    #         print(card1, "beats", card2)
-    #     elif (card2 > card1):
-    #         print(card2, "beats", card1)
+    if card1 == card2:
+        print(card1, "and", card2, "are equal")
+    else:
+        print(card1, "and", card2, "are not equal")
+        if (card1 > card2):
+            print(card1, "beats", card2)
+        elif (card2 > card1):
+            print(card2, "beats", card1)
+    print(card1.__class__.__name__)
 
-    # print(repr(card1))
-    # print(card1.__class__.__name__)
+    print(repr(card1))
+    print(card1.__class__.__name__)
 
     # I'd like to keep counts of identical cards I see
-    # cards_dict = {}
+    cards_dict = {}
 
-    # if card1 in cards_dict:
-    #     cards_dict[card1] += 1
-    # else:
-    #     cards_dict[card1] = 1
+    if card1 in cards_dict:
+        cards_dict[card1] += 1
+    else:
+        cards_dict[card1] = 1
 
-    # if card2 in cards_dict:
-    #     cards_dict[card2] += 1
-    # else:
-    #     cards_dict[card2] = 1
+    if card2 in cards_dict:
+        cards_dict[card2] += 1
+    else:
+        cards_dict[card2] = 1
 
-    # print(cards_dict)
+    print(cards_dict)
+    # What is hashing?
+    # In order to make a dict, we need hashable stuff as keys
+    # Hashing means immutable things have the same trace number
+    # eg. strings, int, tuple are hashable
+    # Lists, dict, and sets are not hashable
 
     # print(hash(card1))
     # print(card1.__hash__())
