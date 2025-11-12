ce8c4d3ff4e8e7182939f562008a3c86ac8650ac
Jiali Sheng <jerry_lovae@Jialis-MacBook-Air.local>
2025-11-12 15:50:35 -0800

    Add code

--- Week11/triangular1.py
+++ Week11/triangular1.py
@@ -7,7 +7,20 @@ def main(number):
     number (sum of natural nums from 0 to number
     Number -> None)
     """
-    pass
+    print(tri_num(number))
+
+
+def tri_num(number):
+    if (number == 0):
+        return 0
+    else:
+        return number + tri_num(number-1)
 
 
 main(int(sys.argv[1]))
+# This recursion works like the stack. The last called function
+# must be solved first in order to finish all other function
+# calls
+# That means the first called function will be stored in the
+# memory until the last called function is solved. This will
+# require lots of memory storage.
--- Week11/triangular2.py
+++ Week11/triangular2.py
@@ -8,7 +8,25 @@ def main(number):
     using tail recursion
     Number -> None)
     """
-    pass
+    print(tri_num(number, 0, 0))
+
+
+def tri_num(number, counter, total):
+    if (counter == number):
+        return total
+    else:
+        counter += 1
+        total = total + counter
+        return tri_num(number, counter, total)
 
 
 main(int(sys.argv[1]))
+# This recursion works not like stack because each
+# call of the function will be excuted with
+# arithmetic and no need to wait for the last
+# call function. This will not take the space in
+# the CPU.
+# Tail recursion: No need to do anything about the
+# last call of the function with previous recursions.
+
+# Tail call optimization
